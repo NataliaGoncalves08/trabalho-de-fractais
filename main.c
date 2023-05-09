95b68a838fa7368ff158d550144a6ac9a02d20ae
Natalia Goncalves <97234051+NataliaGoncalves08@users.noreply.github.com>
2023-05-09 15:50:37 -0300

    funcao 1 implementada

--- main.c
+++ main.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
-char fractais(int op, char regra[15]) {
+char* fractais(int op, char* regra, char* stringf) {
+  char auxiliar[10000];
+  if (op == 0) {
+    return stringf;
+  }
+  else {
+    for (int cont = 0; cont <= strlen(stringf); cont++) {
+      if (stringf[cont] == 'F') {
+        for (int cont2 = 0; cont2 <= strlen(regra); cont2++) {
+          auxiliar[strlen(auxiliar)] = regra[cont2];
+        }
+      }
+      else {
+        auxiliar[strlen(auxiliar)] = stringf[cont];
+      }
+    }
   
+  }
+  return fractais(op - 1, regra, auxiliar);
 }
 
 int main() {
-  int estagios = 0;
-  char rproducao[15] = "F-F+F+FF-F-F+F";
-  scanf("%d", &estagios);
-  fractais(estagios, rproducao);
-  printf("Hello World\n");
+  int estagios = 2;
+  char rproducao[10000] = "F-F+F+FF-F-F+F";
+  char stringfinal[10000];
+  char fina[10000]; 
+  strcpy(fina, fractais(estagios, rproducao, rproducao));
+  printf("%s", fina);
+  printf("Acabou =(");
   return 0;
 }
