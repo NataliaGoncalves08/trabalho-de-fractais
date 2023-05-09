bc05f7053d7a52808a480fa004677d37b8719ce4
Natalia Goncalves <97234051+NataliaGoncalves08@users.noreply.github.com>
2023-05-09 14:43:03 -0300

    Arquivo inicial com a main do 1 fractal
    idem

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+char fractais(int op, char regra[15]) {
+  
+}
+
+int main() {
+  int estagios = 0;
+  char rproducao[15] = "F-F+F+FF-F-F+F";
+  scanf("%d", &estagios);
+  fractais(estagios, rproducao);
+  printf("Hello World\n");
+  return 0;
+}
