6c095de7329f74170500364fa2793edb3d3d41b5
Natalia Goncalves <97234051+NataliaGoncalves08@users.noreply.github.com>
2023-05-17 09:53:02 -0300

    Função 1 finalizada na main

--- main.c
+++ main.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 
-char* fractais(int op, char* regra, char* stringf) {
-  char auxiliar[10000];
-  if (op == 0) {
-    return stringf;
+char* gerar_sequencias(char F, char regra[15], char *string_de_retorno, int estagio_atual, FILE *arquivo_strings) {
+  char string_auxiliar[1000000];
+  if(estagio_atual == 4) {
+    fprintf(arquivo_strings, "Estágio %d: ", estagio_atual);
+    //fprintf(arquivo_strings, "%s\n", string_auxiliar);
+    fputs(string_auxiliar, arquivo_strings);
+    fprintf(arquivo_strings, "\n");
+    
+    return string_de_retorno;
   }
   else {
-    for (int cont = 0; cont <= strlen(stringf); cont++) {
-      if (stringf[cont] == 'F') {
-        for (int cont2 = 0; cont2 <= strlen(regra); cont2++) {
-          auxiliar[strlen(auxiliar)] = regra[cont2];
+    for (long int cont = 0; cont <= strlen(string_de_retorno); cont++) {
+      if (regra[cont] == F) {
+        for (long int cont2 = 0; cont2 <= strlen(regra); cont2++) {
+          string_auxiliar[strlen(string_auxiliar)] = regra[cont2];
         }
       }
       else {
-        auxiliar[strlen(auxiliar)] = stringf[cont];
+        string_auxiliar[strlen(string_auxiliar)] = string_de_retorno[cont];
       }
     }
-  
+    if(estagio_atual < 4) {
+    fprintf(arquivo_strings, "Estágio %d: ", estagio_atual);
+    //fprintf(arquivo_strings, "%s\n", string_auxiliar);
+    fputs(string_auxiliar, arquivo_strings);
+    fprintf(arquivo_strings, "\n");
+    }
   }
-  return fractais(op - 1, regra, auxiliar);
+  return gerar_sequencias(F, regra, string_auxiliar, estagio_atual + 1, arquivo_strings);
 }
 
 int main() {
-  int estagios = 2;
-  char rproducao[10000] = "F-F+F+FF-F-F+F";
-  char stringfinal[10000];
-  char fina[10000]; 
-  strcpy(fina, fractais(estagios, rproducao, rproducao));
-  printf("%s", fina);
-  printf("Acabou =(");
+  //declarações de variáveis
+  char axioma;
+  int graus, estagios = 0;
+  char regra[15];
+  char string_de_retorno[1000000];
+  FILE *pont_estagios;
+
+  //criação e alocação de dados
+  pont_estagios = fopen("estagios.txt", "w");
+  scanf("%c %d %s", &axioma, &graus, regra);
+
+  //chamando funções
+  gerar_sequencias(axioma, regra, string_de_retorno, estagios, pont_estagios); //PARTE 1
+
   return 0;
+  
 }
