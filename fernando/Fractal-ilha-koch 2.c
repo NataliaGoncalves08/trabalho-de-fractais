57c151d9424f72ae681c762801b654ddc65b2952
Fernando Coutinho <117923663+FernandCouto@users.noreply.github.com>
2023-05-30 16:45:32 -0300

    Update Fractal-ilha-koch 2.c
    Correção nos nomes de variaveis e das funções

--- fernando/Fractal-ilha-koch 2.c
+++ fernando/Fractal-ilha-koch 2.c
@@ -14,36 +14,36 @@ typedef struct
 
 typedef struct 
 {
-  char string_de_retorno[1000000];
-  FILE *pont_estagios;
-  int estagios;
+  char StrResult[1000000];
+  FILE *PontArqu;
+  int Estagio;
 
 }processamento_frac;
 
 typedef struct 
 {
 
-  int estagio;
+  int Estagio;
   int Fs;
   int Carac;
 
 }anali_esta;
 
  
-void analise_do_estagio(anali_esta atual[QUANT_ESTA], char* str_ref, int estagio)
+void analise_do_estagio (anali_esta atual[QUANT_ESTA], char* str_ref, int Estagio)
 {
-  atual [estagio - 1].estagio = 0;
-  atual [estagio - 1].Carac = 0;
-  atual [estagio - 1].Fs = 0;
+  atual [Estagio - 1].Estagio = 0;
+  atual [Estagio - 1].Carac = 0;
+  atual [Estagio - 1].Fs = 0;
 
-  atual [estagio - 1].estagio = estagio;
-  atual [estagio - 1].Carac = strlen(str_ref);
+  atual [Estagio - 1].Estagio = Estagio; 
+  atual [Estagio - 1].Carac = strlen(str_ref);
   
   for(int i = 0; i < strlen(str_ref); i++)
   {
     if(str_ref[i] == 'F')
     {
-      atual[estagio - 1].Fs++;
+      atual[Estagio - 1].Fs++;
     }
   }
 }
@@ -51,77 +51,77 @@ void analise_do_estagio(anali_esta atual[QUANT_ESTA], char* str_ref, int estagio
 void imprimir_analise(anali_esta atual[QUANT_ESTA], str_frac inicial)
 {
 
-  printf("\n Numero do fractal requerido: %d\n Axioma inicial: %s \n Angulo: %dº \n Regra de repetição %s \n quantidade de estagios: %d\n ",
+  printf("\n Numero do fractal requerido: %d\n Axioma inicial: %s \n Angulo: %dº \n Regra de repetição %s \n quantidade de estágio : %d\n ",
  inicial.Id, inicial.Axioma, inicial.Ang, inicial.Regra, QUANT_ESTA);
   for(int i = 0; i<QUANT_ESTA; i++)
   {
-    printf("\n Estagio %d: %5d F's - %5d caracteres ", atual[i].estagio, atual[i].Fs, atual[i].Carac);
+    printf("\n Estagio %d: %5d F's - %5d caracteres ", atual[i].Estagio,  atual[i].Fs, atual[i].Carac);
   }
 }
 
-char* gerar_sequenciasF(str_frac p, processamento_frac *atual, anali_esta * caracte) 
+char* gerador_sequencias_fractais(str_frac p, processamento_frac *atual, anali_esta * caracte) 
 {
 
   char string_auxiliar[1000000];
 
   //caso base
-  if(atual->estagios == QUANT_ESTA) {
-    for (long int cont = 0; cont <= strlen(atual->string_de_retorno); cont++) {
-      if (atual->string_de_retorno[cont] == 'F') {
+  if(atual->Estagio  == QUANT_ESTA) {
+    for (long int cont = 0; cont <= strlen(atual->StrResult); cont++) {
+      if (atual->StrResult[cont] == 'F') {
         for (long int cont2 = 0; cont2 <= strlen(p.Regra); cont2++) {
           string_auxiliar[strlen(string_auxiliar)] = p.Regra[cont2];
         }
       }
       else {
-        string_auxiliar[strlen(string_auxiliar)] = atual->string_de_retorno[cont];
+        string_auxiliar[strlen(string_auxiliar)] = atual->StrResult[cont];
       }
     }
-    fprintf(atual->pont_estagios, "Estágio %d: ", atual->estagios);
-analise_do_estagio(caracte, string_auxiliar, atual->estagios);
-    fputs(string_auxiliar, atual->pont_estagios);
+    fprintf(atual->PontArqu , "Estágio %d: ", atual->Estagio );
+analise_do_estagio(caracte, string_auxiliar, atual->Estagio);
+    fputs(string_auxiliar, atual->PontArqu );
 
     return "Fim";
   }
   //primeira geração
-   else if (atual->estagios == 1) {
+   else if (atual->Estagio  == 1) {
      for (int cont = 0; cont < strlen(p.Axioma); cont++) {
-        atual->string_de_retorno[strlen(atual->string_de_retorno)] = p.Axioma[cont];
+        atual->StrResult[strlen(atual->StrResult)] = p.Axioma[cont];
       }
-      fprintf(atual->pont_estagios, "Estágio %d: ", atual->estagios);
-analise_do_estagio(caracte, atual->string_de_retorno, atual->estagios);
-      fputs(atual->string_de_retorno, atual->pont_estagios);
-      fprintf(atual->pont_estagios, "\n");
-      atual->estagios++;
-      return gerar_sequenciasF(p, atual, caracte);
+      fprintf(atual->PontArqu , "Estágio %d: ", atual->Estagio );
+analise_do_estagio(caracte, atual->StrResult, atual->Estagio);
+      fputs(atual->StrResult, atual->PontArqu );
+      fprintf(atual->PontArqu , "\n");
+      atual->Estagio ++;
+      return gerador_sequencias_fractais(p, atual, caracte);
       
   } 
   //geração de caracteres
   else {
-    for (long int cont = 0; cont <= strlen(atual->string_de_retorno); cont++) {
-      if (atual->string_de_retorno[cont] == 'F') {
+    for (long int cont = 0; cont <= strlen(atual->StrResult); cont++) {
+      if (atual->StrResult[cont] == 'F') {
         for (long int cont2 = 0; cont2 <= strlen(p.Regra); cont2++) {
           string_auxiliar[strlen(string_auxiliar)] = p.Regra[cont2];
         }
       }
       else {
-        string_auxiliar[strlen(string_auxiliar)] = atual->string_de_retorno[cont];
+        string_auxiliar[strlen(string_auxiliar)] = atual->StrResult[cont];
       }
     }
     
-    //copia de string_auxiliar para string_de_retorno
+    //copia de string_auxiliar para StrResult
     for(long int cont = 0; cont < strlen(string_auxiliar); cont++) {
-      atual->string_de_retorno[cont] = string_auxiliar[cont];
+      atual->StrResult[cont] = string_auxiliar[cont];
     }
-    atual->string_de_retorno[strlen(atual->string_de_retorno)] = '\0';
+    atual->StrResult[strlen(atual->StrResult)] = '\0';
 
     //gravação no arquivo
-    fprintf(atual->pont_estagios, "Estágio %d: ", atual->estagios);
-analise_do_estagio(caracte, atual->string_de_retorno, atual->estagios);
-    fputs(atual->string_de_retorno, atual->pont_estagios);
-    fprintf(atual->pont_estagios, "\n");
+    fprintf(atual->PontArqu , "Estágio %d: ", atual->Estagio);
+analise_do_estagio(caracte, atual->StrResult, atual->Estagio);
+    fputs(atual->StrResult, atual->PontArqu );
+    fprintf(atual->PontArqu , "\n");
   }
-  atual->estagios++;
-  return gerar_sequenciasF(p, atual, caracte);
+  atual->Estagio ++;
+  return gerador_sequencias_fractais(p, atual, caracte);
 }
 
 int main() 
@@ -129,11 +129,10 @@ int main()
   //declarações de variáveis
   str_frac inicial;
   processamento_frac processamento;
-  anali_esta caracteres_por_estagio[QUANT_ESTA];
+  anali_esta caracteres_por_estagio [QUANT_ESTA];
   
-  processamento.estagios = 1;
-  //onde serão escritas as strings de cada estagio
-  //FILE *regras; //tirar na versão final, serve apenas pra teste
+  processamento.Estagio  = 1;
+  //onde serão escritas as strings de cada Estagio   //FILE *regras; //tirar na versão final, serve apenas pra teste
 
   //atribuição de valores
   printf(" Insira o número do axioma desejado: ");
@@ -146,10 +145,10 @@ int main()
   scanf("\n%[^\n]", inicial.Regra);
 
   //abertura de arquivo
-  processamento.pont_estagios = fopen("estagios_fractal.txt", "w");
+  processamento.PontArqu = fopen("Sequencias resultantes.txt", "w");
 
   //chamar função
-  gerar_sequenciasF(inicial, &processamento, caracteres_por_estagio);
+  gerador_sequencias_fractais(inicial, &processamento, caracteres_por_estagio) ;
 
   imprimir_analise(caracteres_por_estagio, inicial);
 
